5f73fef4a003eb82e610db814d8e9d08cb089a27
Patricia Norton <patrick.norton3@btinternet.com>
2025-07-19 15:24:10 +0100

    Added guessing logic to backend

--- wordle-but-crazy_backend/index.js
+++ wordle-but-crazy_backend/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+const Colour = {
+  Grey: 'Grey',
+  Yellow: 'Yellow',
+  Green: 'Green'
+}
+
+let guesses = 0
+let targetWord = 'horse'
+
+app.post('/api/makeguess', (req, res) => {
+  const guess = req.body.word
+  guesses ++
+
+  let newPastGuess = {
+    number: guesses,
+    word: guess,
+    colours: new Array(5).fill(Colour.Grey)
+  }
+
+  if (guess === targetWord)
+    newPastGuess.colours = new Array(5).fill(Colour.Green)
+  else {
+    for (let i = 0; i < 5; i++) {
+      const targetChar = targetWord.charAt(i)
+
+      if (guess.charAt(i) === targetChar)
+        newPastGuess.colours[i] = Colour.Green
+      else if (guess.includes(targetChar))
+        newPastGuess.colours[guess.indexOf(targetChar)] = Colour.Yellow
+    }
+  }
+
+  res.json(newPastGuess)
 })
 
 const PORT = 3001
